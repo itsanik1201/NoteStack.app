eaf0a28eeab09be4bf3fe588010b854bf8b9f944
aniket patel <mailofaniketpatel@gmail.com>
2025-12-11 14:36:01 +0000

    url

--- back/index.js
+++ back/index.js
@@ -8,7 +8,7 @@ const action = require('./Routes/Actions');
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://note-stack-app.vercel.app',
+    origin: '*',
     credentials: true 
 }))
 
